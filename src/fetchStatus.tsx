bd7465c543577d8838cbe1acd3f2e45c22ef45ed
kendpr <real.kendpr@gmail.com>
2024-04-13 01:04:04 +0800

    Conditionally show the status message with CSS instead of JS

--- src/fetchStatus.tsx
+++ src/fetchStatus.tsx
@@ -1,11 +1,12 @@
 export function Status({ status, text }: { status: string; text: string }) {
   return (
     <>
-      {status !== "Ok" ? (
-        <div className="fixed left-0 top-0 z-50 grid h-screen w-full place-items-center bg-slate-900 text-xl font-medium text-slate-400 opacity-60">
-          <span>{text}</span>
-        </div>
-      ) : null}
+      <div
+        data-isnotok={text != null && status !== "Ok" ? "true" : "false"}
+        className="data-isnotok:grid data-isnotok:z-50 fixed left-0 top-0 -z-50  hidden h-screen w-full place-items-center bg-slate-900 text-xl font-medium text-slate-400 opacity-60"
+      >
+        <span>{text}</span>
+      </div>
     </>
   );
 }
